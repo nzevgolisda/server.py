63f8bc74cdb3538c4be405507294b64ea683cf07
nikos zevgolis <nzevgolisda@gmail.com>
2021-10-03 15:19:18 +0300

    Render an HTML document

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 _website
 
 1. `pip install flask`
-2.`go to http://127.0.0.1:8000/`
+3. `python app.py`
+2. go to `http://127.0.0.1:8000/`
--- views.py
+++ views.py
@@ -1,6 +1,12 @@
 
-from flask import Blueprint
+from flask import Blueprint, render_template
 views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
-    return "home page"
+    ## Here arter the self = index.html object
+    ## we can set parameters like name = "name", 
+    ## age = "age" etc, and render them in the html
+    return render_template("index.html", name="Nikos", age="29") 
+
+#def home2():
+#    return "home page"
