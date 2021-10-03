b3e0aa1c6c4e1f317183ecaaa53b752c1c414300
nikos zevgolis <nzevgolisda@gmail.com>
2021-10-03 21:03:32 +0300

    Access parameters in url

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-_website
+server.py
 
 1. `pip install flask`
-3. `python app.py`
-2. go to `http://127.0.0.1:8000/`
+2. `python app.py`
+3. go to `http://http://127.0.0.1:8000/profile/John` to access parameters in url
+4. `pip install --upgrade Flask`
--- views.py
+++ views.py
@@ -3,10 +3,7 @@ from flask import Blueprint, render_template
 views = Blueprint(__name__, "views")
 @views.route("/")
 def home():
-    ## Here arter the self = index.html object
-    ## we can set parameters like name = "name", 
-    ## age = "age" etc, and render them in the html
-    return render_template("index.html", name="Nikos", age="29") 
-
-#def home2():
-#    return "home page"
+    return render_template("index.html", name="Nikos") 
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html", name=username) 
